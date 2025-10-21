71bb591a2d6569bb7f853af741624a1c0056d70b
farman1304 <farmanullah1107@gmail.com>
2025-10-21 23:12:28 +0500

    updated

--- app.js
+++ app.js
@@ -1,45 +1,51 @@
-    var getValue = document.getElementById ('digits');
+    var recData = document.getElementById ('digits');
     var result = document.getElementById ('result');
 
     function getData (data) {
 
-        
-        getValue.value += data
+      if(result.value === 'Error') {
+        // deleteValue ()
+        recData.value = recData.value.slice (0 , -1);
+        result.value = '';
+    }
+    recData.value += data;
 
-        console.log(data)
-        }
+        
+
+    }
+        console.log(recData.value)
 
 
         function clearData () {
 
-            getValue.value = '';
+            recData.value= '';
             // result.value = '';
 
         }
 
         function deleteValue () {
-            getValue.value = getValue.value.slice (0,-1);
+            recData.value= recData.value.slice (0,-1);
         }
 
+        var isOkay = false;
         function calculateNow () {
             var opt = ['/','+','-', '%','*','.'];
 
-            var newText = getValue.value;
-
-            for(var i = 0 ; i < opt.length; i++)  {
-                if(
-                    newText.startsWith(opt[i]) ||
-                    newText.endsWith(opt[i])  ||
-                    newText.includes (opt[i] + opt [i])) {
-                        result.value = 'Error';
-                        return;
-                    }  
+            for(var i = 0 ; i < opt.length; i++) {
+                if(recData.value[0] === opt [i] || recData.value [recData.value.length -1] === opt[i] ||
+                    recData.value === opt [i] + opt[i] ) {
                 
-            } 
-
-            result.value = eval(newText)
-
+                    result.value = 'Error'
+                    isOkay = true;
+                    return;
+                }
             }
-           
+            var finalResult = eval (recData.value);
+            
+            result.value = finalResult;
+        } 
+        
+
+      
 
 
